a30157e8690698f8473c4789e6b3242e9a46dcaf
chrislu <chrislu@igs.com.tw>
2017-08-18 17:58:08 +0800

    add files

---
+++ src/components/Test2.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <h1>
+    Test2
+  </h1>
+
+</template>
+
+
+<style src="../Sass/lib1.scss" scoped></style>
+
+<style lang= "scss">
+  @import 'bourbon';
+  h1{
+    font-weight: bolder;
+    /* @include hide-text; */
+  }
+
+</style>
