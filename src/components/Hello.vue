1ff394cfd8f0254897cf751809486fb3894ab282
chrislu <chrislu@igs.com.tw>
2017-08-23 14:44:39 +0800

    修改結構

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <img src="../assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -38,7 +39,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-// @import 'bourbon';
+.hello {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
 h1, h2 {
   font-weight: normal;
 }
---
+++ src/pages/Cash.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cash">123</div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
